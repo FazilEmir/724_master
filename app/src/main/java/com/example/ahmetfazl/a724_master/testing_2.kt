719d883923ed41a7a57526221e09758e5b3f904a
Ahmet Fazıl Emir <ahmtfazl@gmail.com>
2018-02-22 14:50:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ahmetfazl/a724_master/testing_2.kt
@@ -0,0 +1,12 @@
+package com.example.ahmetfazl.a724_master
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class testing_2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing_2)
+    }
+}
